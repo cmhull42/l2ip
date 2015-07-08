7c50a0fe356b982f3867c18ca870d919be0dec1a
cmhull42 <cmhull42@gmail.com>
2015-07-08 11:15:25 -0500

    Restructure to work as lib, add documentation

--- l2ip.py
+++ l2ip.py
@@ -2,20 +2,39 @@
 from __future__ import print_function
 import requests, sys, argparse, re
 
-# Eventually may move to a separate config file
-LOG_PORT = 9200
-API_URL = "http://example.com:{port}/logstash-{date}/iptables/{id}"
-IPTABLES_COMMAND = "IPTABLES -A INPUT -p {proto} -m {proto} -s {source} --dport {dport} -m state --state NEW -j ACCEPT"
+class ElasticSearchMessage:
+	IPTABLES_COMMAND = "IPTABLES -A INPUT -p {proto} -m {proto} -s {source} --dport {dport} -m state --state NEW -j ACCEPT"
+	""" represents a message returned from elasticsearch"""
+	def __init__(self, message):
+		self.message = message
 
-# ---- End config ---- 
 
-def retrieveMessageJson(date, id):
-	resp = requests.get(API_URL.format(date=date, id=id, port=LOG_PORT))
-	if resp.status_code == 404:
-		print("No such messageID {} on {}".format(id, date), file=sys.stderr)
-		exit(1)
+	def toIptables(self):
+		"""returns the iptables command or None if the message doesn't contain the required information"""
 
-	return resp.json()
+		extractor = re.compile("SRC=(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}).*?PROTO=(TCP|UDP).*?DPT=(\d{1,4})")
+		match = extractor.search(self.message["_source"]["message"])
+		if match is None:
+			return None
+
+		return self.IPTABLES_COMMAND.format(source=match.group(1), proto=match.group(2), dport=match.group(3))
+
+class LogRetriever:
+	"""handles querying elasticsearch for messages. apiUrl should have named format groups date, id, and port
+		 see API_URL for an example"""
+	def __init__(self, port, apiUrl):
+		self.port = port
+		self.apiUrl = apiUrl
+
+	def retrieveMessageJson(self, date, id):
+		"""makes a request to apiUrl for the given date and id
+			 returns the response as JSON or None if not found"""
+		resp = requests.get(self.apiUrl.format(date=date, id=id, port=self.port))
+		if resp.status_code == 404:
+			return None
+
+		return resp.json()
+
 
 def main():
 	parser = argparse.ArgumentParser(description="Parses logs from elasticsearch into iptables commands.")
@@ -23,15 +42,18 @@ def main():
 	parser.add_argument("-i", dest="id", required=True, help="The elastisearch messageID")
 	args = parser.parse_args()
 
-	logMessage = retrieveMessageJson(args.date, args.id)["_source"]["message"]
-	extractor = re.compile("SRC=(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}).*?PROTO=(TCP|UDP).*?DPT=(\d{1,4})")
-	match = extractor.search(logMessage)
+	logRetriever = LogRetriever(LOG_PORT, API_URL)
+	fullMessage = logRetriever.retrieveMessageJson(args.date, args.id)
+	match = ElasticSearchMessage(fullMessage).toIptables()
+
 	if match is None:
 		print("Couldn't match the data returned from ElasticSearch.", file=sys.stderr)
 		exit(1)
 
-	print(IPTABLES_COMMAND.format(source=match.group(1), proto=match.group(2), dport=match.group(3)))
+	print(match)
 
 
 if __name__ == "__main__":
+	LOG_PORT = 80
+	API_URL = "http://demo4341911.mockable.io:{port}/logstash-{date}/iptables/{id}"
 	main()
